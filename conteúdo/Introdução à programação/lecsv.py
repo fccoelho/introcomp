34bdc0e4a9d31001a67a7735f3afb2ba8d3f7c4b
Flávio Codeço Coelho <fccoelho@gmail.com>
2023-05-24 09:12:22 -0300

    Adicionei rascunho com trechos de código útil para realização do Projeto 1

--- conteúdo/Introdução à programação/lecsv.py
+++ conteúdo/Introdução à programação/lecsv.py
@@ -1,20 +1,21 @@
 import argparse
 
+
 def le_arquivo(nome_arq):
-    with open(nome_arq,'r') as f:
-        linhas = [tuple(l.strip().split('","')) \
-                  for l in f.readlines()[4:]]
+    with open(nome_arq, 'r') as f:
+        linhas = [tuple(l.strip().strip('"').split('","')) for l in f.readlines()[4:]]
     # print(linhas[:2])
     lcab = len(linhas[0])
     for n, l in enumerate(linhas):
         try:
             assert len(l) == lcab
         except AssertionError:
-            print (n, len(l))
-        # print (l) 
+            print(n, len(l))
+        # print (l)
+    print(f"{n} linhas processadas do arquivo {nome_arq}")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Lê um arquivo CSV')
     parser.add_argument("csv", type=open, help="Arquivo CSV")
     # print(dir(parser.parse_args()))
