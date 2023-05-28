949d7f25bb2ffade76e6fd11249accef654453b2
Flávio Codeço Coelho <fccoelho@gmail.com>
2023-05-28 15:42:14 -0300

    fixed formatting

--- Avaliações/AS/test_as.py
+++ Avaliações/AS/test_as.py
@@ -1,12 +1,12 @@
-    """
-    Testes para o módulo AS.
-    """
-    import AS
+"""
+Testes para o módulo AS.
+"""
+import AS
+
+def test_autor():
+    assert isinstance(AS.AUTOR, str)
+    assert len(AS.AUTOR) == 1
     
-    def test_autor():
-        assert isinstance(AS.AUTOR, str)
-        assert len(AS.AUTOR) == 1
-        
-    def test_numquestions():
-        for i in range(5):
-            assert hasattr(AS, f'questao_{i+1}')
+def test_numquestions():
+    for i in range(5):
+        assert hasattr(AS, f'questao_{i+1}')
